d320530efb79e2b2b2bcb9e38101d3079e9b93f1
Greg Brisebois <gregoryjjb@gmail.com>
2018-03-28 23:53:06 -0700

    Hover style for tags

--- src/components/PostLink.js
+++ src/components/PostLink.js
@@ -35,7 +35,21 @@ const PostLink = ({ node }) => (
 					textDecoration: 'none',
 					':hover': {textDecoration: 'none'}
 				}} >
-					<p css={{color: 'lightgrey'}} >Tags: {(node.frontmatter.tags || []).join(', ')}</p>
+					<p css={{color: 'lightgrey'}} >Tags: {(node.frontmatter.tags || []).map(tag => (
+						<span>
+							<span css={{
+								':hover': {
+									color: 'white',
+									background: 'salmon',
+									borderRadius: '0.3em',
+									padding: '0.1em 0.25em',
+								}
+							}} >
+								{tag}
+							</span>
+							{', '}
+						</span>
+					))}</p>
 					<p>{node.excerpt}</p>
 				</span>
 			</div>
