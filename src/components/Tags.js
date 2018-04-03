8fafc3b3b8abd9c5df74403a222e579fa722d4ee
Greg <gregoryjjb@gmail.com>
2018-04-02 17:12:14 -0700

    Aesthetic changes

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -4,9 +4,7 @@ import _ from 'lodash'
 
 // Thanks to Sophie Alpert from KhanAcademy
 const intersperse = (arr, sep) => {
-    if (arr.length === 0) {
-        return [];
-    }
+    if (arr.length === 0) { return [] }
 
     return arr.slice(1).reduce(function(xs, x, i) {
         return xs.concat([sep, x]);
