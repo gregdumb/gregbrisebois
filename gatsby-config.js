16d959b3132bde1696fbfbf5048f9632959dd2a3
Greg Brisebois <gregoryjjb@gmail.com>
2018-04-22 18:10:19 -0700

    Added g favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,24 @@ module.exports = {
 				respectDNT: true,
 			}
 		},
+		{
+			resolve: `gatsby-plugin-favicon`,
+			options: {
+				logo: "./src/favicon.png",
+				injectHTML: true,
+				icons: {
+					android: true,
+					appleIcon: true,
+					appleStartup: true,
+					coast: false,
+					favicons: true,
+					firefox: true,
+					twitter: false,
+					yandex: false,
+					windows: false
+				}
+			}
+		},
 		'gatsby-plugin-sharp',
 		'gatsby-transformer-sharp',
 	],
