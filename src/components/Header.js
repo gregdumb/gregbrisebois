e8852c50f8b9f9eeae1429fd99aa657919484b92
Greg Brisebois <gregoryjjb@gmail.com>
2018-06-02 09:58:55 -0700

    Replaced header with appbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { withStyles } from '@material-ui/core/styles';
+
 import Link from 'gatsby-link'
 import { colors, sizes, media } from '../theme'
 import Container from './Container'
@@ -61,97 +69,31 @@ const DropDown = ({ children, visible }) => (
 	</div>
 )
 
-const Header = ({ headerTitle, toggleDropDown, closeDropDown, dropDownVisible }) => (
-	<header css={{
-		background: colors.veryDark,
-		color: colors.veryDarkText,
-		width: '100%',
-		position: 'fixed',
-		zIndex: 1,
-		top: 0,
-		left: 0,
-	}} >
-		<Container>
-			<div css={{
-				display: 'flex',
-				flexDirection: 'row',
-				alignItems: 'center',
-				height: sizes.header.large,
-				[media.lessThan('large')]: {
-					height: sizes.header.small
-				}
-			}} >
-				<h1 css={{
-					margin: 0,
-					display: 'inline',
-					fontSize: '30px',
-					whiteSpace: 'nowrap',
-					[media.lessThan('large')]: {
-						fontSize: '25px',
-					}
-				}} >
-					<Link
-						to="/"
-						css={{
-							color: 'white',
-							textDecoration: 'none',
-							':hover': {
-								textDecoration: 'none',
-							}
-						}} >
-						{headerTitle}
-					</Link>
-				</h1>
-				<div css={{
-					display: 'flex',
-					alignItems: 'center',
-					overflowY: 'auto',
-					height: '100%',
-					paddingLeft: '2em',
-					[media.lessThan('small')]: {
-						/*paddingLeft: '0.5em',
-						paddingRight: '0.5em',
-						maskImage: 'linear-gradient(to right, transparent, black 20px, black 90%, transparent)',*/
-						display: 'none',
-					}
-				}}>
-					<HeaderLink text="Home" path="/" />
-					<HeaderLink text="Projects" path="/projects/" />
-					<HeaderLink text="Tutorials" path="/tutorials/" />
-					<HeaderLink text="About" path="/about/" />
-				</div>
-				<a onClick={toggleDropDown} css={{
-					display: 'none',
-					color: colors.veryDarkText,
-					marginLeft: 'auto',
-					zIndex: 4,
-					[media.lessThan('small')]: {
-						display: 'block',
-					}
-				}} >
-					<Hamburger isActive={dropDownVisible} />
-				</a>
-			</div>
-			<DropDown visible={dropDownVisible}>
-				<HeaderLink text="Home" path="/" onClick={toggleDropDown} />
-				<HeaderLink text="Projects" path="/projects/" onClick={toggleDropDown} />
-				<HeaderLink text="Tutorials" path="/tutorials/" onClick={toggleDropDown} />
-				<HeaderLink text="About" path="/about/" onClick={toggleDropDown} />
-			</DropDown>
-			<div onClick={closeDropDown} css={{
-				//display: dropDownVisible ? 'block' : 'none',
-				background: 'black',
-				opacity: dropDownVisible ? 0.5 : 0,
-				zIndex: 3,
-				position: 'fixed',
-				top: 0,
-				bottom: 0,
-				left: 0,
-				right: 0,
-				transition: 'all 0.1s ease-in-out',
-				pointerEvents: dropDownVisible ? 'auto' : 'none',
-			}} />
-		</Container>
+const styles = {
+	root: {
+		flexGrow: 1,
+	},
+	title: {
+		flex: 1,
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+	}
+}
+
+const Header = ({ classes, headerTitle, toggleDropDown, closeDropDown, dropDownVisible }) => (
+	<header className={classes.root} >
+		<AppBar position="static" color="default" >
+			<Toolbar>
+				<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+					<MenuIcon />
+				</IconButton>
+				<Typography className={classes.title} variant="title" color="inherit" >
+					Greg Brisebois
+				</Typography>
+			</Toolbar>
+		</AppBar>
 	</header>
 )
 
@@ -159,4 +101,4 @@ Header.propTypes = {
 	headerTitle: PropTypes.string
 }
 
-export default Header
+export default withStyles(styles)(Header);
