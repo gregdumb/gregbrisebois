469be90f7f1a68c5ca9a24443397272f5aea0d43
Greg Brisebois <gregoryjjb@gmail.com>
2018-06-09 13:20:37 -0700

    Converted skill cards to new MD cards

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,19 +2,32 @@ import React from 'react'
 import PageContent from '../components/PageContent'
 import { styles } from '../theme'
 
-const SkillGroup = ({ children, name }) => (
-    <div css={{
-        marginBottom: '2em',
-    }} >
-        <h3>{name}</h3>
-        <div css={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-        }} >
-            {children}
-        </div>
-    </div>
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+
+import Container from '../components/Container'
+
+const SkillGroup = ({ skills, name }) => (
+	<div style={{marginBottom: 32}} >
+		<Typography variant="title" gutterBottom>{name}</Typography>
+		<Grid container spacing={24} >
+			{skills.map(skill => (
+				<Grid item xs={4} sm={3} md={2} lg={2} >
+					<Card >
+						<div style={{ padding: skill.padding || 0 }} >
+							<CardMedia image={skill.image} title={skill.name} style={{paddingTop: '100%'}} />
+						</div>
+						<CardContent style={{padding: 8}} >
+							<Typography noWrap variant='body1' align="center" >{skill.name}</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+			))}
+		</Grid>
+	</div>
 )
 
 const Skill = ({ name, image, padding }) => (
@@ -39,8 +52,102 @@ const Skill = ({ name, image, padding }) => (
     </div>
 )
 
+const skillGroups = [{
+	group: "Languages",
+	skills: [{
+		name: "Javascript",
+		image: "/img/logos/js-logo.jpg",
+	}, {
+		name: "HTML5",
+		image: "/img/logos/html5-logo.svg",
+		padding: '10%',
+	}, {
+		name: "C++",
+		image: "/img/logos/cplusplus-logo.svg",
+		padding: '5%',
+	}, {
+		name: "Java",
+		image: "/img/logos/java-logo.jpg",
+		padding: '5%',
+	}, {
+		name: "Python",
+		image: "/img/logos/python-logo.svg",
+		padding: '10%',
+	}]
+}, {
+	group: "Frontend",
+	skills: [{
+		name: "React",
+		image: "/img/logos/react-logo.jpg",
+	}, {
+		name: "Redux",
+		image: "/img/logos/redux-logo.jpg",
+		padding: '10%',
+	}, {
+		name: "Gatsby",
+		image: "/img/logos/gatsby-logo.jpg",
+		padding: '10%',
+	}, {
+		name: "jQuery",
+		image: "/img/logos/jquery-logo.jpg",
+		padding: '10%',
+	}, {
+		name: "Android",
+		image: "/img/logos/android-logo.jpg",
+		padding: "10%",
+	}]
+}, {
+	group: "Server Frameworks & Libraries",
+	skills: [{
+		name: "Express",
+		image: "/img/logos/express-logo.jpg",
+		padding: "5%",
+	}, {
+		name: "Sequelize",
+		image: "/img/logos/sequelize-logo.jpg",
+		padding: "5%",
+	}, {
+		name: "Socket.io",
+		image: "/img/logos/socket-logo.jpg"
+	}]
+}, {
+	group: "Server Technologies",
+	skills: [{
+		name: "Node.js",
+		image: "/img/logos/node-logo.jpg",
+		padding: "5%",
+	}, {
+		name: "NGINX",
+		image: "/img/logos/nginx-logo.jpg",
+		padding: "5%",
+	}, {
+		name: "Apache",
+		image: "/img/logos/apache-logo.jpg",
+		padding: "10%",
+	}, {
+		name: "MySQL",
+		image: "/img/logos/mysql-logo.jpg",
+		padding: "10%",
+	}, {
+		name: "Ubuntu Server",
+		image: "/img/logos/ubuntu-logo.jpg",
+		padding: "10%",
+	}]
+}, {
+	group: "Game Development",
+	skills: [{
+		name: "Unreal Engine",
+		image: "/img/logos/unreal-logo.jpg",
+		padding: "10%",
+	}, {
+		name: "Blender",
+		image: "/img/logos/blender-logo.jpg",
+		padding: "10%"
+	}]
+}]
+
 const About = () => (
-    <PageContent>
+    <Container>
         <h1>About me</h1>
 		<h2>
 			<a href="https://apps.gregbrisebois.com/downloads/gregbriseboisresume.pdf" target="_blank" >
@@ -62,39 +169,12 @@ const About = () => (
             </li>
         </ul>
         <h2>Skills</h2>
-        <SkillGroup name={"Languages"} >
-            <Skill name="Javascript" image={'/img/logos/js-logo.jpg'} />
-            <Skill name="HTML5" image={'/img/logos/html5-logo.svg'} padding={'10%'} />
-            <Skill name="C++" image={'/img/logos/cplusplus-logo.svg'} padding={'5%'} />
-            <Skill name="Java" image={'/img/logos/java-logo.jpg'} padding={'5%'} />
-            <Skill name="Python" image={'/img/logos/python-logo.svg'} padding={'10%'} />
-        </SkillGroup>
-        <SkillGroup name={"Frontend"} >
-            <Skill name="React" image={'/img/logos/react-logo.jpg'} />
-            <Skill name="Redux" image={'/img/logos/redux-logo.jpg'} padding={'10%'} />
-            <Skill name="Gatsby" image={'/img/logos/gatsby-logo.jpg'} padding={'10%'} />
-            <Skill name="jQuery" image={'/img/logos/jquery-logo.jpg'} padding={'10%'} />
-            <Skill name="Android" image={'/img/logos/android-logo.jpg'} padding={'10%'} />
-        </SkillGroup>
-        <SkillGroup name={"Server Frameworks/Libraries"} >
-            <Skill name="Express" image={'/img/logos/express-logo.jpg'} padding={'5%'} />
-            <Skill name="Sequelize" image={'/img/logos/sequelize-logo.jpg'} padding={'5%'} />
-            <Skill name="Socket.io" image={'/img/logos/socket-logo.jpg'} padding={'0'} />
-        </SkillGroup>
-		<SkillGroup name={"Server Technologies"} >
-			<Skill name="Node.js" image={'/img/logos/node-logo.jpg'} padding={'5%'} />
-			<Skill name="Apache" image={'/img/logos/apache-logo.jpg'} padding={'10%'} />
-			<Skill name="NGINX" image={'/img/logos/nginx-logo.jpg'} padding={'5%'} />
-            <Skill name="MySQL" image={'/img/logos/mysql-logo.jpg'} padding={'10%'} />
-			<Skill name="Ubuntu Server" image={'/img/logos/ubuntu-logo.jpg'} padding={'10%'} />
-		</SkillGroup>
-        <SkillGroup name={"Game Development"} >
-            <Skill name="Unreal Engine" image={'/img/logos/unreal-logo.jpg'} padding={'10%'} />
-            <Skill name="Blender" image={'/img/logos/blender-logo.jpg'} padding={'10%'} />
-        </SkillGroup>
+		{skillGroups.map(group => (
+			<SkillGroup skills={group.skills} name={group.group} />
+		))}
         <h2>About the Site</h2>
         <p>This site was built with React and Gatsby</p>
-    </PageContent>
+    </Container>
 )
 
 export default About;
