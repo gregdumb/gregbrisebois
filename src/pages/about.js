84af48e505ad4a26e53bc433c28d5ad16510bfdd
Greg Brisebois <gregoryjjb@gmail.com>
2018-06-16 09:46:33 -0700

    Moved skill data to separate content file

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,6 +11,8 @@ import Button from '@material-ui/core/Button'
 import TitledPage from '../components/TitledPage'
 import Experience from '../components/Experience'
 
+import { skills, experience } from '../content/skills'
+
 const SkillGroup = ({ skills, name }) => (
 	<div style={{marginBottom: 32}} >
 		<Typography variant="title" gutterBottom>{name}</Typography>
@@ -30,107 +32,6 @@ const SkillGroup = ({ skills, name }) => (
 		</Grid>
 	</div>
 )
-
-const skillGroups = [{
-	group: "Languages",
-	skills: [{
-		name: "Javascript",
-		image: "/img/logos/js-logo.jpg",
-	}, {
-		name: "HTML5",
-		image: "/img/logos/html5-logo.svg",
-		padding: '10%',
-	}, {
-		name: "C++",
-		image: "/img/logos/cplusplus-logo.svg",
-		padding: '5%',
-	}, {
-		name: "Java",
-		image: "/img/logos/java-logo.jpg",
-		padding: '5%',
-	}, {
-		name: "Python",
-		image: "/img/logos/python-logo.svg",
-		padding: '10%',
-	}]
-}, {
-	group: "Frontend",
-	skills: [{
-		name: "React",
-		image: "/img/logos/react-logo.jpg",
-	}, {
-		name: "Redux",
-		image: "/img/logos/redux-logo.jpg",
-		padding: '10%',
-	}, {
-		name: "Gatsby",
-		image: "/img/logos/gatsby-logo.jpg",
-		padding: '10%',
-	}, {
-		name: "jQuery",
-		image: "/img/logos/jquery-logo.jpg",
-		padding: '10%',
-	}, {
-		name: "Android",
-		image: "/img/logos/android-logo.jpg",
-		padding: "10%",
-	}]
-}, {
-	group: "Server Frameworks & Libraries",
-	skills: [{
-		name: "Express",
-		image: "/img/logos/express-logo.jpg",
-		padding: "5%",
-	}, {
-		name: "Sequelize",
-		image: "/img/logos/sequelize-logo.jpg",
-		padding: "5%",
-	}, {
-		name: "Socket.io",
-		image: "/img/logos/socket-logo.jpg"
-	}]
-}, {
-	group: "Server Technologies",
-	skills: [{
-		name: "Node.js",
-		image: "/img/logos/node-logo.jpg",
-		padding: "5%",
-	}, {
-		name: "NGINX",
-		image: "/img/logos/nginx-logo.jpg",
-		padding: "5%",
-	}, {
-		name: "Apache",
-		image: "/img/logos/apache-logo.jpg",
-		padding: "10%",
-	}, {
-		name: "MySQL",
-		image: "/img/logos/mysql-logo.jpg",
-		padding: "10%",
-	}, {
-		name: "Ubuntu Server",
-		image: "/img/logos/ubuntu-logo.jpg",
-		padding: "10%",
-	}]
-}, {
-	group: "Game Development",
-	skills: [{
-		name: "Unreal Engine",
-		image: "/img/logos/unreal-logo.jpg",
-		padding: "10%",
-	}, {
-		name: "Blender",
-		image: "/img/logos/blender-logo.jpg",
-		padding: "10%"
-	}]
-}]
-
-const experience = [{
-	company: "Analog Devices",
-	jobTitle: "Mixed Signal Intern",
-	dates: "July 2017 - Present",
-	description: "Worked on multiple projects, including: <ul><li>Web-bsaed design aid tools</li><li>Linduino (Arduino) driver development for ADI parts</li><li>Desktop .NET plugins for ACE testing platform</li></ul>",
-}]
 
 const About = () => (
     <TitledPage title="About Me" >
@@ -155,7 +56,7 @@ const About = () => (
 		</Grid>
 		
         <Typography variant="display2" gutterBottom >Skills</Typography>
-		{skillGroups.map(group => (
+		{skills.map(group => (
 			<SkillGroup skills={group.skills} name={group.group} key={group.group} />
 		))}
 		
