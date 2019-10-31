141b70f138f7b9f80ad572967d5ee22a90a722a9
Greg Brisebois <gregoryjjb@gmail.com>
2019-10-31 00:20:57 -0700

    Add Triplebyte experience

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,140 +1,158 @@
 import React from 'react'
 import { StaticQuery, graphql, Link } from 'gatsby'
 
-import { withStyles, Typography, Grid, Divider, Button } from '@material-ui/core'
-import EmailIcon from '@material-ui/icons/Email';
-import FilePdfIcon from 'mdi-material-ui/FilePdf';
+import {
+  withStyles,
+  Typography,
+  Grid,
+  Divider,
+  Button,
+} from '@material-ui/core'
+import EmailIcon from '@material-ui/icons/Email'
+import FilePdfIcon from 'mdi-material-ui/FilePdf'
 
-import { experience } from '../content/skills';
-import ShowcaseCard from './ShowcaseCard';
-import Experience from './Experience';
-import UnstyledLink from './UnstyledLink';
+import { experience } from '../content/skills'
+import ShowcaseCard from './ShowcaseCard'
+import Experience from './Experience'
+import UnstyledLink from './UnstyledLink'
 
-import profilePic from '../content/img/profile-round.png';
+import profilePic from '../content/img/profile-round.png'
 
 const projectsQuery = graphql`
-    {
-        allMarkdownRemark(
-            filter: {
-                frontmatter: { featured: { eq: true } }
-                fields: { folder: { eq: "projects" } }
-            }
-        ) {
-            edges {
-                node {
-                    frontmatter {
-                        title
-                        date
-                        tags
-                        featured
-                        summary
-                        image {
-                            childImageSharp {
-                                fluid(maxWidth: 400) {
-                                    src
-                                    base64
-                                }
-                            }
-                        }
-                    }
-                    fields {
-                        slug
-                    }
-                    excerpt
+  {
+    allMarkdownRemark(
+      filter: {
+        frontmatter: { featured: { eq: true } }
+        fields: { folder: { eq: "projects" } }
+      }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+            tags
+            featured
+            summary
+            image {
+              childImageSharp {
+                fluid(maxWidth: 400) {
+                  src
+                  base64
                 }
+              }
             }
+          }
+          fields {
+            slug
+          }
+          excerpt
         }
+      }
     }
+  }
 `
 
 const styles = theme => ({
-    root: {
-        marginTop: 32,
-        marginBottom: 72,
-    },
-    profile: {
-        //width: 128,
-        display: 'block',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-    },
-    sectionTitle: {
-        //marginTop: 48,
-    },
-    projectsGrid: {
-        marginTop: 16,
-        marginBottom: 16,
-    },
-    button: {
-        marginRight: 16,
-        marginBottom: 16,
-    },
-    icon: {
-        marginRight: 8,
-    }
+  root: {
+    marginTop: 32,
+    marginBottom: 72,
+  },
+  profile: {
+    //width: 128,
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  sectionTitle: {
+    //marginTop: 48,
+  },
+  projectsGrid: {
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  button: {
+    marginRight: 16,
+    marginBottom: 16,
+  },
+  icon: {
+    marginRight: 8,
+  },
 })
 
-const divider = name => <>
+const divider = name => (
+  <>
     <Divider style={{ margin: '32px 0' }} />
-    <Typography variant='h4' gutterBottom>{name}</Typography>
-</>;
+    <Typography variant="h4" gutterBottom>
+      {name}
+    </Typography>
+  </>
+)
 
 const Portfolio = ({ classes }) => (
-    <div className={classes.root} >
-        <img src={profilePic} className={classes.profile} />
-        {divider('Featured Projects')}
-        <StaticQuery
-            query={projectsQuery}
-            render={({ allMarkdownRemark: { edges } }) => (
-                <Grid container spacing={32} className={classes.projectsGrid} >
-                    {edges.map(({ node }) => (
-                        <Grid xs={12} item>
-                            <ShowcaseCard node={node} />
-                        </Grid>
-                    ))}
-                </Grid>
-            )}
-        />
-        <Typography variant='body1' gutterBottom>
-            See more projects <Link to='/projects'>here</Link> or on
-            <a href='https://github.com/gregoryjjb' target='_blank' rel='noreferrer'> my GitHub</a>.
-        </Typography>
-        {divider('Experience')}
-        {experience.map(e => <Experience experience={e} />)}
-        {divider('Skills')}
-        <Typography variant='body1'>In order of proficiency:</Typography>
-        <Typography variant='body1'>
-            <ul>
-                <li>Web fundamentals (HTML5, ES6, CSS)</li>
-                <li>Modern frontend development (React, JSS, Webpack)</li>
-                <li>Server-side development (Nodejs, SQL, WebSockets)</li>
-                <li>C++ (Arduino, Unreal Engine)</li>
-            </ul>
-        </Typography>
-        {divider('Contact')}
-        <Button
-            variant='outlined'
-            size='large'
-            color='primary'
-            component='a'
-            href='mailto:gregoryjjb@gmail.com'
-            target='_blank'
-            className={classes.button}>
-            <EmailIcon className={classes.icon}/>
-            Email
-        </Button>
-        <Button
-            variant='outlined'
-            size='large'
-            color='primary'
-            component='a'
-            href='https://apps.gregbrisebois.com/downloads/gregbriseboisresume.pdf'
-            target='_blank'
-            className={classes.button}>
-            <FilePdfIcon className={classes.icon} />
-            Download Résumé
-        </Button>
-    </div>
+  <div className={classes.root}>
+    <img src={profilePic} className={classes.profile} />
+    {divider('Featured Projects')}
+    <StaticQuery
+      query={projectsQuery}
+      render={({ allMarkdownRemark: { edges } }) => (
+        <Grid container spacing={32} className={classes.projectsGrid}>
+          {edges.map(({ node }) => (
+            <Grid xs={12} item>
+              <ShowcaseCard node={node} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    />
+    <Typography variant="body1" gutterBottom>
+      See more projects <Link to="/projects">here</Link> or on
+      <a href="https://github.com/gregoryjjb" target="_blank" rel="noreferrer">
+        {' '}
+        my GitHub
+      </a>
+      .
+    </Typography>
+    {divider('Experience')}
+    {experience.map(e => (
+      <Experience experience={e} />
+    ))}
+    {divider('Skills')}
+    <Typography variant="body1">In order of proficiency:</Typography>
+    <Typography variant="body1">
+      <ul>
+        <li>Web fundamentals (HTML5, ES6, CSS)</li>
+        <li>Modern frontend development (React, JSS, Webpack)</li>
+        <li>Server-side development (Nodejs, SQL, WebSockets)</li>
+        <li>C++ (Arduino, Unreal Engine)</li>
+      </ul>
+    </Typography>
+    {divider('Contact')}
+    <Button
+      variant="outlined"
+      size="large"
+      color="primary"
+      component="a"
+      href="mailto:gregoryjjb@gmail.com"
+      target="_blank"
+      className={classes.button}
+    >
+      <EmailIcon className={classes.icon} />
+      Email
+    </Button>
+    <Button
+      variant="outlined"
+      size="large"
+      color="primary"
+      component="a"
+      href="https://apps.gregbrisebois.com/downloads/gregbriseboisresume.pdf"
+      target="_blank"
+      className={classes.button}
+    >
+      <FilePdfIcon className={classes.icon} />
+      Download Résumé
+    </Button>
+  </div>
 )
 
 export default withStyles(styles)(Portfolio)
--- src/content/skills.js
+++ src/content/skills.js
@@ -1,131 +1,178 @@
+export const skills = [
+  {
+    group: 'Languages',
+    skills: [
+      {
+        name: 'Javascript',
+        image: '/img/logos/js-logo.jpg',
+      },
+      {
+        name: 'HTML5',
+        image: '/img/logos/html5-logo.svg',
+        padding: '10%',
+      },
+      {
+        name: 'C++',
+        image: '/img/logos/cplusplus-logo.svg',
+        padding: '5%',
+      },
+      {
+        name: 'C#',
+        image: '/img/logos/csharp-logo.jpg',
+        padding: '1%',
+      },
+      {
+        name: 'Java',
+        image: '/img/logos/java-logo.jpg',
+        padding: '5%',
+      },
+      {
+        name: 'Python',
+        image: '/img/logos/python-logo.svg',
+        padding: '10%',
+      },
+    ],
+  },
+  {
+    group: 'Frontend',
+    skills: [
+      {
+        name: 'React',
+        image: '/img/logos/react-logo.jpg',
+      },
+      {
+        name: 'Redux',
+        image: '/img/logos/redux-logo.jpg',
+        padding: '10%',
+      },
+      {
+        name: 'Webpack',
+        image: '/img/logos/webpack-logo.jpg',
+      },
+      {
+        name: 'Gatsby',
+        image: '/img/logos/gatsby-logo.jpg',
+        padding: '10%',
+      },
+      {
+        name: 'jQuery',
+        image: '/img/logos/jquery-logo.jpg',
+        padding: '10%',
+      },
+      {
+        name: 'Android',
+        image: '/img/logos/android-logo.jpg',
+        padding: '10%',
+      },
+    ],
+  },
+  {
+    group: 'Server Frameworks & Libraries',
+    skills: [
+      {
+        name: 'Express',
+        image: '/img/logos/express-logo.jpg',
+        padding: '5%',
+      },
+      {
+        name: 'Sequelize',
+        image: '/img/logos/sequelize-logo.jpg',
+        padding: '5%',
+      },
+      {
+        name: 'Socket.io',
+        image: '/img/logos/socket-logo.jpg',
+      },
+    ],
+  },
+  {
+    group: 'Server Technologies',
+    skills: [
+      {
+        name: 'Node.js',
+        image: '/img/logos/node-logo.jpg',
+        padding: '5%',
+      },
+      {
+        name: 'NGINX',
+        image: '/img/logos/nginx-logo.jpg',
+        padding: '5%',
+      },
+      {
+        name: 'Apache',
+        image: '/img/logos/apache-logo.jpg',
+        padding: '10%',
+      },
+      {
+        name: 'MySQL',
+        image: '/img/logos/mysql-logo.jpg',
+        padding: '10%',
+      },
+      {
+        name: 'Ubuntu Server',
+        image: '/img/logos/ubuntu-logo.jpg',
+        padding: '10%',
+      },
+    ],
+  },
+  {
+    group: 'Game Development',
+    skills: [
+      {
+        name: 'Unreal Engine',
+        image: '/img/logos/unreal-logo.jpg',
+        padding: '10%',
+      },
+      {
+        name: 'Blender',
+        image: '/img/logos/blender-logo.jpg',
+        padding: '10%',
+      },
+    ],
+  },
+  {
+    group: 'Tools',
+    skills: [
+      {
+        name: 'Git',
+        image: '/img/logos/git-logo.svg',
+        padding: '10%',
+      },
+      {
+        name: 'VS Code',
+        image: '/img/logos/vscode-logo.jpg',
+        padding: '10%',
+      },
+      {
+        name: 'Visual Studio',
+        image: '/img/logos/vs-logo.jpg',
+        padding: '10%',
+      },
+      {
+        name: 'Travis',
+        image: '/img/logos/travis-logo.jpg',
+        padding: '10%',
+      },
+      {
+        name: 'Jenkins',
+        image: '/img/logos/jenkins-logo.jpg',
+        padding: '5%',
+      },
+    ],
+  },
+]
 
-export const skills = [{
-	group: "Languages",
-	skills: [{
-		name: "Javascript",
-		image: "/img/logos/js-logo.jpg",
-	}, {
-		name: "HTML5",
-		image: "/img/logos/html5-logo.svg",
-		padding: '10%',
-	}, {
-		name: "C++",
-		image: "/img/logos/cplusplus-logo.svg",
-		padding: '5%',
-	}, {
-		name: "C#",
-		image: "/img/logos/csharp-logo.jpg",
-		padding: '1%',
-	}, {
-		name: "Java",
-		image: "/img/logos/java-logo.jpg",
-		padding: '5%',
-	}, {
-		name: "Python",
-		image: "/img/logos/python-logo.svg",
-		padding: '10%',
-	}]
-}, {
-	group: "Frontend",
-	skills: [{
-		name: "React",
-		image: "/img/logos/react-logo.jpg",
-	}, {
-		name: "Redux",
-		image: "/img/logos/redux-logo.jpg",
-		padding: '10%',
-	}, {
-		name: "Webpack",
-		image: "/img/logos/webpack-logo.jpg",
-	}, {
-		name: "Gatsby",
-		image: "/img/logos/gatsby-logo.jpg",
-		padding: '10%',
-	}, {
-		name: "jQuery",
-		image: "/img/logos/jquery-logo.jpg",
-		padding: '10%',
-	}, {
-		name: "Android",
-		image: "/img/logos/android-logo.jpg",
-		padding: "10%",
-	}]
-}, {
-	group: "Server Frameworks & Libraries",
-	skills: [{
-		name: "Express",
-		image: "/img/logos/express-logo.jpg",
-		padding: "5%",
-	}, {
-		name: "Sequelize",
-		image: "/img/logos/sequelize-logo.jpg",
-		padding: "5%",
-	}, {
-		name: "Socket.io",
-		image: "/img/logos/socket-logo.jpg"
-	}]
-}, {
-	group: "Server Technologies",
-	skills: [{
-		name: "Node.js",
-		image: "/img/logos/node-logo.jpg",
-		padding: "5%",
-	}, {
-		name: "NGINX",
-		image: "/img/logos/nginx-logo.jpg",
-		padding: "5%",
-	}, {
-		name: "Apache",
-		image: "/img/logos/apache-logo.jpg",
-		padding: "10%",
-	}, {
-		name: "MySQL",
-		image: "/img/logos/mysql-logo.jpg",
-		padding: "10%",
-	}, {
-		name: "Ubuntu Server",
-		image: "/img/logos/ubuntu-logo.jpg",
-		padding: "10%",
-	}]
-}, {
-	group: "Game Development",
-	skills: [{
-		name: "Unreal Engine",
-		image: "/img/logos/unreal-logo.jpg",
-		padding: "10%",
-	}, {
-		name: "Blender",
-		image: "/img/logos/blender-logo.jpg",
-		padding: "10%"
-	}]
-}, {
-	group: "Tools",
-	skills: [{
-		name: "Git",
-		image: "/img/logos/git-logo.svg",
-		padding: "10%",
-	}, {
-		name: "VS Code",
-		image: "/img/logos/vscode-logo.jpg",
-		padding: "10%",
-	}, {
-		name: "Visual Studio",
-		image: "/img/logos/vs-logo.jpg",
-		padding: "10%",
-	}, {
-		name: "Travis",
-		image: "/img/logos/travis-logo.jpg",
-		padding: "10%",
-	}, {
-		name: "Jenkins",
-		image: "/img/logos/jenkins-logo.jpg",
-		padding: '5%',
-	}]
-}]
-
-export const experience = [{
-	company: "Analog Devices",
-	jobTitle: "Mixed Signal Intern",
-	dates: "July 2017 - August 2018",
-	description: "Worked on multiple projects, including: <ul><li>Web-based design aid tools</li><li>Linduino (Arduino) driver development for ADI parts</li><li>Desktop .NET plugins for ACE testing platform</li></ul>",
-}]
+export const experience = [
+  {
+    company: 'Triplebyte',
+    jobTitle: 'Software Engineer',
+    dates: 'March 2019 - Present',
+  },
+  {
+    company: 'Analog Devices',
+    jobTitle: 'Mixed Signal Intern',
+    dates: 'July 2017 - March 2019',
+    description:
+      'Worked on multiple projects, including: <ul><li>Web-based design aid tools</li><li>Linduino (Arduino) driver development for ADI parts</li><li>Desktop .NET plugins for ACE testing platform</li></ul>',
+  },
+]
